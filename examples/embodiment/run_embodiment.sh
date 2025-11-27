2efdd3ad995f7dd2dfa28a0741c77a9b2a1c6ee5
Tengbo-Yu <ytb23@mails.tsinghua.edu.cn>
2025-11-27 05:58:33 +0000

    update

--- examples/embodiment/run_embodiment.sh
+++ examples/embodiment/run_embodiment.sh
@@ -4,11 +4,17 @@ export EMBODIED_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export REPO_PATH=$(dirname $(dirname "$EMBODIED_PATH"))
 export SRC_FILE="${EMBODIED_PATH}/train_embodied_agent.py"
 
-export MUJOCO_GL="egl"
-export PYOPENGL_PLATFORM="egl"
+# export MUJOCO_GL="egl"
+# export PYOPENGL_PLATFORM="egl"
+
+export MUJOCO_GL="osmesa"
+export PYOPENGL_PLATFORM="osmesa"
+
+# fix "AttributeError: 'MjRenderContextOffscreen' object has no attribute 'con'"
+# export DISPLAY=:0
 
 # NOTE: set LIBERO_REPO_PATH to the path of the LIBERO repo
-export LIBERO_REPO_PATH="/opt/libero"
+export LIBERO_REPO_PATH="/workspace/data"
 
 export PYTHONPATH=${REPO_PATH}:${LIBERO_REPO_PATH}:$PYTHONPATH
 
@@ -23,7 +29,6 @@ export OMNIGIBSON_HEADLESS=${OMNIGIBSON_HEADLESS:-1}
 export ISAAC_PATH=${ISAAC_PATH:-/path/to/isaac-sim}
 export EXP_PATH=${EXP_PATH:-$ISAAC_PATH/apps}
 export CARB_APP_PATH=${CARB_APP_PATH:-$ISAAC_PATH/kit}
-
 
 if [ -z "$1" ]; then
     CONFIG_NAME="maniskill_ppo_openvlaoft"
